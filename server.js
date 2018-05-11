b43c830c9147f7c9275465e4346773c5dee2fb4a
Collin <rocket3989@gmail.com>
2018-05-11 07:58:49 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = app.listen(80);
+var users = [];
+app.use(express.static('public'));
+
+console.log('server open on port 8080');
+
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection',newConnection);
+function newConnection(client) {
+	var id = users.length;
+	console.log('user '+id);
+	client.emit('name',{name:id});
+	users.push(socket);
+	client.on('data',data =>{ 
+		console.log(data);
+		client.broadcast.emit('data',data);
+		});
+	client.on('disconnect',function(data){
+		console.log("connection end");
+	});
+}
